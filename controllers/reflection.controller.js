2bde7d7dcc1b7c948b7457529b1c2917fc3fc7de
shanjita <shanjita18si@mahasiswa.pcr.ac.id>
2022-04-20 22:47:56 +0700

    add put, delete, get api

--- controllers/reflection.controller.js
+++ controllers/reflection.controller.js
@@ -25,3 +25,68 @@ exports.postReflection = async (req, res) => {
       });
     });
 };
+
+exports.getReflection = async (req, res) => {
+  return Reflection.findAll().then(reflection=> {
+    res.status(201).send({
+        status : "SUCCES",
+        data: reflection
+        })
+    })
+    .catch(e => {
+    console.log(e)
+    res.status(503).send({
+        status : "FAIL",
+        message : 'Gagal menampilkan refleksi'
+      });
+    });
+};
+
+exports.deleteReflection = async(req, res) => {
+  const id = req.params.id
+  return Reflection.destroy( {where : {id:id}} ).then (reflection => {
+    res.status(201).json({
+      "data": "sukses",
+       })
+    })  
+      .catch(e => {
+          console.log(e)
+          res.status(503).json({
+              "message": "INTERNAL SERVER ERROR"
+          });
+      });
+};
+
+  exports.putReflection = async(req, res) => {
+      const id = req.params.id      
+      let success = req.body.success;
+      let low_point = req.body.low_point;
+      let take_away = req.body.take_away;
+   
+      const user_id = req.user_id
+        let data = {
+        success,
+        low_point,
+        take_away,
+        user_id,
+      };
+        return Reflection.update(data,{
+          where: {
+            id: id
+          }
+        } 
+        )
+        .then(hasil => {
+                res.status(201).send({
+                    "status": "Berhasil mengupdate reflection",
+                    "data" : hasil,
+  
+                });
+            })
+            .catch(e => {
+                console.log(e)
+                res.status(503).send({
+                    "message": "Gagal mengupdate reflection"
+                });
+            });
+          };
--- routes/reflection.js
+++ routes/reflection.js
@@ -4,4 +4,8 @@ const controller = require("../controllers/reflection.controller");
 const auth = require("../middlewares/auth");
 
 router.post("/", auth.verify, controller.postReflection);
+router.get("/", auth.verify, controller.getReflection);
+router.put("/:id", auth.verify, controller.putReflection);
+router.delete("/:id", auth.verify, controller.deleteReflection);
+
 module.exports = router;
