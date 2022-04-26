eecc2208f52196f20aa5871a19a1ccfe590a72ce
James Kirawan <jameskirawan@gmail.com>
2022-04-26 19:51:31 +0700

    refine config.js

--- config/config.js
+++ config/config.js
@@ -8,7 +8,7 @@ module.exports = {
     dialect: "postgres",
   },
   production: {
-    username: process.env.DB_USER,
+    user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
